c79991bc4bdb2da2619d09cb7cf354da92425f31
Rick Paramanik <rickkcir100@gmail.com>
2025-02-07 17:56:44 +0530

    socket initialized

--- server/index.js
+++ server/index.js
@@ -1,13 +1,16 @@
 const express = require("express")
 require('dotenv').config()
-
-
+const http = require("http");
+const { Server } = require("socket.io");
 const app = express();
+
+const server = http.createServer(app);
+
 
 app.get("/",(req,res)=>{
     res.send("Server running fine!!!!!!")
 })
 
-app.listen(process.env.PORT,()=>{
+server.listen(process.env.PORT,()=>{
     console.log(`SERVER RUNNING AT PORT ${process.env.PORT}`);
 })
