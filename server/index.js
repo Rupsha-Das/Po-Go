a12f9672da5334e6a7d06bb1983df6049738b29c
Viraj Anand <iamtheviraj@gmail.com>
2025-02-09 00:24:47 +0530

    serverfile

--- server/index.js
+++ server/index.js
@@ -1,210 +1,217 @@
-const express = require("express")
-const cors = require("cors")
-require('dotenv').config()
+const WebSocket = require('ws');
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
 const http = require("http");
-const { Server } = require("socket.io");
 const app = express();
-const User = require("./user.model.js")
-const mongoose = require("mongoose")
+const User = require("./user.model.js");
+const mongoose = require("mongoose");
 
+// const app = express();
 const server = http.createServer(app);
+const wss = new WebSocket.Server({ server });
 
-app.use(express.json())
-app.use(
-    cors({
-        origin: true,
-        credentials: true,
-    })
-);
-
-app.get("/",(req,res)=>{
-    res.send("Server running fine!!!!!!")
-})
-
-
+// Maps to track connections
 const producers = new Map();
 const consumers = new Map();
 const consumerEmails = new Map();
-// producers.set(4444,null)
 
-function printPC(){
-    console.log("producers\n___________");
-    for(const [key,val] of producers){
-        console.log(`${key} - ${val}`);
-    }
-    console.log("consumers\n___________");
-    for(const [key,val] of consumers){
-        console.log(`${key} - ${val}`);
-    }
-    console.log("emails\n___________");
-    for(const [key,val] of consumerEmails){
-        console.log(`${key} - ${val}`);
-    }
-}
+app.use(express.json());
+app.use(cors({
+  origin: true,
+  credentials: true,
+}));
 
-
-const io = new Server(server, {
-    cors: {
-        origin: "*",
-        credentials: true,
-    },
+app.get("/", (req, res) => {
+  res.send("Server running fine!!!!!!");
 });
 
+function printPC() {
+  console.log("producers\n___________");
+  for (const [key, val] of producers) {
+    console.log(`${key} - ${val}`);
+  }
+  console.log("consumers\n___________");
+  for (const [key, val] of consumers) {
+    console.log(`${key} - ${val}`);
+  }
+  console.log("emails\n___________");
+  for (const [key, val] of consumerEmails) {
+    console.log(`${key} - ${val}`);
+  }
+}
+
+wss.on('connection', (ws) => {
+  const clientId = Math.random().toString(36).substring(7);
+  console.log(`Client connected: ${clientId}`);
+
+  ws.on('message', async (message) => {
+    try {
+      const data = JSON.parse(message);
+      console.log('Received:\n' + JSON.stringify(data, null, 2));
+
+      switch (data.action) {
+        case 'register':
+          if (data.type === 'producer') {
+
+            // dev_id_temp <- (from mongo)
+            // dev_id_temp++ if posture.overall == bad else dev_id_temp--
+            // if dev_id_temp > threashold:
+            //   send email to user
+            // push updated dev_id_temp to mongo
 
 
-io.on('connection',(socket)=>{
-  console.log(`user connected - ${socket.id}`)
-  socket.on('register',(deviceInfo)=>{
-    if(deviceInfo.type==='producer'){
-        producers.set(socket.id,null); 
-    }else{//consumer
-        consumers.set(socket.id,null);
-        consumerEmails.set(socket.id,deviceInfo.email);
+            producers.set(clientId, null);
+          } else {
+            consumers.set(clientId, null);
+            consumerEmails.set(clientId, data.email);
+          }
+          ws.send(JSON.stringify({
+            action: 'register_response',
+            status: 'success',
+            clientId: clientId
+          }));
+          break;
+
+        case 'connect_to_producer':
+          let connected = false;
+          for (const [producerId, consumerId] of producers) {
+            if (consumerId === null) {
+              producers.set(producerId, clientId);
+              consumers.set(clientId, producerId);
+              connected = true;
+              console.log(`Consumer:${clientId} Connected to Producer:${producerId}`);
+              break;
+            }
+          }
+          ws.send(JSON.stringify({
+            action: 'connect_to_producer_result',
+            status: connected ? 'success' : 'failure',
+            message: connected ? 'Connected to Device' : 'Unable to Connect'
+          }));
+          break;
+
+        case 'send_data_to_consumer':
+          const consumerId = producers.get(clientId);
+          const consumerEmail = consumerEmails.get(consumerId);
+          console.log(`consumerId:${consumerId} and consumerEmail:${consumerEmail}`);
+          
+          let result = await User.findOne({ email: consumerEmail });
+          let msg;
+          
+          if (!result) {
+            msg = "User not exists!!";
+          } else {
+            await User.updateOne(
+              { email: consumerEmail },
+              {
+                $set: { posture: [...data.posture, ...result.posture] },
+              }
+            );
+            msg = "Successful update";
+          }
+          
+          ws.send(JSON.stringify({
+            action: 'posture_data_update',
+            status: result ? 'success' : 'failure',
+            message: msg
+          }));
+          break;
+
+        default:
+          ws.send(JSON.stringify({
+            action: 'error',
+            message: 'Unknown action'
+          }));
+      }
+    } catch (error) {
+      console.error('Error processing message:', error);
+      ws.send(JSON.stringify({
+        action: 'error',
+        message: 'Invalid message format'
+      }));
     }
-    console.log(deviceInfo.type, socket.id)
-  })
+  });
 
-  socket.on('connect-to-producer',()=>{
+  ws.on('close', () => {
+    console.log(`Client disconnected: ${clientId}`);
+    
+    // Clean up maps
+    if (producers.has(clientId)) {
+      producers.delete(clientId);
+    }
+    
     for (const [producerId, consumerId] of producers) {
-        // console.log(`producesss  ${producerId} ${consumerId}`)
-        if (consumerId == null) {
-            producers.set(producerId,socket.id);
-            consumers.set(socket.id,producerId);
-            console.log(`Consumer:${socket.id} Connected to Producer:${producerId} `)
-            break;
-        }
+      if (consumerId === clientId) {
+        producers.set(producerId, null);
+      }
     }
-    if(!consumers.get(socket.id))console.log("Unable to Connect")
-    socket.emit('connect-to-producer-result',{
-        msg:consumers.get(socket.id)?"Connected to Device":"Unable to Connect"
-    })
-  })
-
-  socket.on('send-data-to-consumer',async(data)=>{    
-    console.log(data)//data received from viraj side
-    printPC();
-    const consumerId = producers.get(socket.id);
-    const consumerEmail = consumerEmails.get(consumerId);
-    printPC()
-    console.log(`consumerId:${consumerId} and consumerEmail:${consumerEmail}`)
-    let result  = await User.findOne({email:consumerEmail});
-    console.log(result)
-    let msg;
-    if(!result){
-        msg="User not exists!!"
-    }else{
-        await User.updateOne({email:consumerEmail},{
-            $set:{posture:[...data,...result.posture]}
-        })
-        msg="Successful update";
-    }
-    console.log(msg);
-    socket.emit('posture-data-update',{msg});
     
-  })
+    if (consumers.has(clientId)) {
+      consumers.delete(clientId);
+    }
+    
+    if (consumerEmails.has(clientId)) {
+      consumerEmails.delete(clientId);
+    }
+  });
+});
+
+// Keep the Express routes
+app.get("/login", async (req, res) => {
+  const { email, password } = req.body;
+  let result = await User.findOne({ email });
+
+  if (result && result.password === password) {
+    res.send({
+      status: 200,
+      msg: "Success!!",
+      data: result,
+    });
+  } else {
+    res.send({
+      status: 400,
+      msg: "Invalid credentials!!",
+      data: null,
+    });
+  }
+});
+
+app.post("/signin", async (req, res) => {
+  const { name, email, password } = req.body;
+  let result = await User.findOne({ email });
   
-  socket.on('disconnect',()=>{
-    for (const [producerId, consumerId] of producers) {
-        if (producerId == socket.id ) {
-            producers.set(producerId,null);break;
-        }
-        if (consumerId == socket.id ) {
-            producers.set(producerId,null);break;
-        }
+  if (result) {
+    res.send({
+      status: 400,
+      msg: "User already registered!!",
+      data: null,
+    });
+  } else {
+    const userObj = new User({
+      name,
+      email,
+      password,
+    });
+
+    result = await userObj.save();
+
+    if (result) {
+      res.send({
+        status: 200,
+        msg: "Success!!",
+        data: result,
+      });
+    } else {
+      res.send({
+        status: 400,
+        msg: "Failed!!",
+        data: null,
+      });
     }
-    for (const [consumerId, producerId] of consumers) {
-        if (producerId == socket.id ) {
-            consumers.set(producerId,null);break;
-        }
-        if (consumerId == socket.id ) {
-            consumers.set(producerId,null);break;
-        }
-    }
-  })
-  
-})
+  }
+});
 
-
-
-app.get('/login',async (req,res)=>{
-    const {email,password}=req.body;
-    let result  = await User.findOne({email});
-    
-    if(result && result.password === password){
-        res.send({
-            status:200,
-            msg:"Success!!",
-            data:result
-        })
-    }else {
-        res.send({
-            status:400,
-            msg:"Invalid credentials!!",
-            data:null
-        })
-    }
-    
-    
-})
-app.post('/signin',async (req,res)=>{
-    const {name,email,password}=req.body;
-    let result  = await User.findOne({email});
-    if(result){
-        res.send({
-            status:400,
-            msg:"User already registered!!",
-            data:null
-        })
-    }else{
-
-        const userObj = new User({
-            name,
-            email,
-            password
-        })
-    
-        result  = await userObj.save();
-    
-        if(result){
-            res.send({
-                status:200,
-                msg:"Success!!",
-                data:result
-            })
-        }else{
-            res.send({
-                status:400,
-                msg:"Failed!!",
-                data:null
-            })
-        }
-    }
-    
-})
-// app.post('/update-posture',async (req,res)=>{
-//     const {machineId,posture}=req.body;
-//     // const email = machineToUserEmail.get(machineId);
-//     const email="rick@gmail.com";
-//     let result  = await User.findOne({email});
-    
-//     if(result){
-//         await User.updateOne({email},{
-//             $set:{posture:[...posture,...result.posture]}
-//         })
-//         res.send({
-//             status:200,
-//             msg:"Success!!",
-//         })
-//     }else {
-//         res.send({
-//             status:400,
-//             msg:"Failed!!"
-//         })
-//     }
-// })
-
-
-
-server.listen(process.env.PORT,()=>{
-    console.log(`SERVER RUNNING AT PORT ${process.env.PORT}`);
-})
+server.listen(process.env.PORT, () => {
+  console.log(`Server running on port ${process.env.PORT}`);
+});
